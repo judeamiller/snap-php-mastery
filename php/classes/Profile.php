5b9f0bddd024497779feec86970bf64c1b81b2c3
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-27 09:05:10 -0600

    add mutators and accessors for profile email

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -31,6 +31,13 @@ class Profile {
 	public function getProfileFavoriteNumber() {
 		return ($this->profileFavoriteNumber);
 	}
+	
+		/**
+	 * @param mixed $profileFavoriteNumber
+	 */
+	public function setProfileFavoriteNumber($profileFavoriteNumber) {
+		$this->profileFavoriteNumber = $profileFavoriteNumber;
+	}
 
 	/**
 	 * @return mixed
@@ -40,9 +47,48 @@ class Profile {
 	}
 
 	/**
+	 * @param mixed $profileEmail
+	 */
+	public function setProfileEmail(string $newProfileEmail) : void {
+		//verify author email is secure
+		$newProfileEmail = trim($newProfileEmail);
+		$newProfileEmail = filter_var($newProfileEmail, FILTER_VALIDATE_EMAIL);
+		if($newProfileEmail === true) {
+			throw(new \InvalidArgumentException("author email is empty or insecure"));
+		}
+
+		//verify that the email will fit in database
+		if(strlen($newProfileEmail) > 128) {
+			throw(new \RangeException("profile email is greater than 128 characters"));
+		}
+
+		//convert and store the author email
+		$this->profileEmail = $newProfileEmail;
+	}
+
+	/**
 	 * @return mixed
 	 */
 	public function getProfileJoinDate() {
 		return ($this->profileJoinDate);
+	}
+
+	/**
+	 * @param mixed $profileJoinDate
+	 */
+	public function setProfileJoinDate($newProfileJoinDate = null): void {
+		//base case: if the date is null, use current date and time
+		if($newProfileJoinDate === null) {
+			$this->profileJoinDate = new \DateTime();
+			return;
+		}
+		// store date using validateTrait
+		try {
+			$newProfileJoinDate = self::validateDateTime($newProfileJoinDate);
+		} catch(\InvalidArgumentException | \RangeException | \ Exception $exception){
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(),0, $exception));
+		}
+		$this->profileJoinDate = $newProfileJoinDate;
 	}
 }
